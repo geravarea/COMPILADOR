a3ff36c65a5648686dcf82d32ec0285789bbf388
geravarea <38798334+geravarea@users.noreply.github.com>
2019-01-02 17:49:16 -0600

    Update Compilador2475.java

--- Compilador2475.java
+++ Compilador2475.java
@@ -6,10 +6,13 @@
 package compilador2475;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.StringTokenizer;
+
+
 
 /**
  *
@@ -17,57 +20,61 @@ import java.util.StringTokenizer;
  */
 public class Compilador2475 {
     static Diccionario diccionario= new Diccionario();
+    
+  
+        
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-          File entrada;
+        File entrada;
         entrada = new File("C:\\Users\\Gerardo Vargas\\Desktop\\NetBeans\\entrada.txt");
         
-      try {
-            BufferedReader leer = new BufferedReader(new FileReader(entrada));
-            String linea=leer.readLine();
-           
-            StringTokenizer tokens=new StringTokenizer(linea,";");
-          
-            //display.setText(linea);
-           //System.out.print(linea);
-          
-           String tok;
-           
-          for(int i=0; i<26; i++){
-            while(tokens.hasMoreTokens()){
+            try {
+                BufferedReader leer = new BufferedReader(new FileReader(entrada));
+                String linea=leer.readLine();
+
+                String s=(linea);
+                String[]tokens=s.split("[;]");
+               
+               int i;
+               int x;
+                for( x=0; x<tokens.length; x++){
+                    boolean bandera=false;
+                    for(i=0; i<26; i++){
+                      //System.out.println(diccionario.getValor(x));  
+                      
+                        String regex =(diccionario.getValor(i)); 
+                          //System.out.println(diccionario.getValor(x));
+                        if(tokens[x].equals(regex)){  
+                            bandera=true;
+                            break;
+                        }
+                       
+                    }
+                     if(bandera==true){
+                            System.out.println("Token: "+tokens[x]+" = "+diccionario.getValor2(i)); 
+                        try (FileWriter escritura = new FileWriter("C:\\Users\\Gerardo Vargas\\Desktop\\NetBeans\\salida.lib")) {
+                            escritura.write("Token: "+diccionario.getValor2(i));
+                             escritura.write("\n");
+                        }
+                                
+                        }else{  
+                            
+                            System.out.println("Error: la expresion "+"'"+tokens[x]+"'"+" no existe."+diccionario.getValor2(i));
+                                
+                    }
+                     
+            
+                   
+                }
                 
-            tok=tokens.nextToken();
-             
-            String regex =(diccionario.getValor(i));
-            //System.out.println(regex);
-            
-            if(tok.matches(regex)){
-                System.out.println("Token: "+tok+" = "+diccionario.getValor2(i));
-            }else{
-            
-          System.out.println("Error: la expresion "+"'"+tok+"'"+" no existe.");
-      
-            }
-            
-            } 
-            //System.out.println("token: "+tok);
-            i=i+1;
-            }
-          
-            
-            //JOptionPane.showMessageDialog(null,linea);
-            
-        } catch (IOException ex) {
-           
-        }
-        // TODO code application logic here
-        
-    
+           }catch (IOException ex){}
+             // TODO code application logic here 
     }
+    
 }
+
     
    
     
