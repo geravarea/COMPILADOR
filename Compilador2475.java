e1f6f83defe6e77d4f27affb9dcdf38e9bb92406
geravarea <38798334+geravarea@users.noreply.github.com>
2019-01-04 17:41:09 -0600

    Update Compilador2475.java

--- Compilador2475.java
+++ Compilador2475.java
@@ -5,66 +5,119 @@
  */
 package compilador2475;
 
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
  * @author Gerardo Vargas
  */
 public class Compilador2475 {
-    static Diccionario diccionario= new Diccionario();
-    
-  
-        
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        File entrada;
-        entrada = new File("C:\\Users\\Gerardo Vargas\\Desktop\\NetBeans\\entrada.txt");
-        
-            try {
-                BufferedReader leer = new BufferedReader(new FileReader(entrada));
-                String linea=leer.readLine();
 
-                String s=(linea);
-                String[]tokens=s.split("[;]");
-               
-               int i;
-               int x;
-                for( x=0; x<tokens.length; x++){
-                    boolean bandera=false;
-                    for(i=0; i<26; i++){
-                      //System.out.println(diccionario.getValor(x));  
-                      
-                        String regex =(diccionario.getValor(i)); 
-                          //System.out.println(diccionario.getValor(x));
-                        if(tokens[x].equals(regex)){  
-                            bandera=true;
+    static Diccionario diccionario = new Diccionario();
+    static DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    static DateFormat hourFormat = new SimpleDateFormat("HH:mm:ss");
+    static Date date = new Date();
+
+    public static void main(String[] args) {
+        System.out.println("-------------DOCUMENTO PROCESADO----------------");
+        System.out.println("FECHA:" + dateFormat.format(date));
+        System.out.println("HORA:" + hourFormat.format(date));
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+
+        File entrada;
+        entrada = new File("entrada.txt");
+        //Se lee lo que contiene el archivo entrada.txt y se guarda en el String linea
+        try {
+            BufferedReader leer = new BufferedReader(new FileReader(entrada));
+            String linea = leer.readLine();
+
+            //usamos el metodo split para separar con tokens el contenido del String linea 
+            String[] tokens = linea.split("[,\\s]+");
+            String sFichero = "salida.txt";
+            File escritura = new File(sFichero);
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(sFichero))) {
+                bw.write("------------------------------------------------");
+                bw.newLine();
+                bw.write("             DOCUMENTO PROCESADO                ");
+                bw.newLine();
+                bw.write("------------------------------------------------");
+                bw.newLine();
+                bw.newLine();
+                bw.newLine();
+                bw.write("------------------------------------------------");
+                bw.newLine();
+                bw.write("FECHA:" + dateFormat.format(date) + "                " + "HORA:" + hourFormat.format(date));
+                bw.newLine();
+                bw.write("------------------------------------------------");
+                //bw.newLine();
+                //bw.write("HORA:" +hourFormat.format(date));
+                //bw.newLine();
+                bw.newLine();
+                //bw.write("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                bw.newLine();
+                int i;
+                int x;
+                //Expresion regular para numeros y decimales
+                String regex2 = ("^[0-9]*(\\.[0-9])?$+");
+                //Expresion regular para las variables
+                String regex3 = ("[A-Z]+");
+                //Este ciclo recorre los tokens uno por uno
+                for (x = 0; x < tokens.length; x++) {
+                    boolean bandera = false;
+                    //Este siclo recorre todo el diccionario de simbolos
+                    for (i = 0; i < 26; i++) {
+
+                        String regex = (diccionario.getValor(i));
+
+                        //si alguno de los tokens es igual a algun valor del diccionario
+                        if (tokens[x].equals(regex) || tokens[x].matches(regex2) || tokens[x].matches(regex3)) {
+
+                            bandera = true;
                             break;
                         }
-                       
                     }
-                     if(bandera==true){
-                            System.out.println("Token: "+tokens[x]+" = "+diccionario.getValor2(i)); 
-                        try (FileWriter escritura = new FileWriter("C:\\Users\\Gerardo Vargas\\Desktop\\NetBeans\\salida.lib")) {
-                            escritura.write("Token: "+diccionario.getValor2(i));
-                             escritura.write("\n");
+                    if (bandera == true) {
+
+                        if (tokens[x].matches(regex2)) {
+                            System.out.println("Token: valor " + tokens[x]);
+                            bw.write("Token: valor " + tokens[x]);
+                            bw.newLine();
+
+                        } else if (tokens[x].matches(regex3)) {
+
+                            System.out.println("Token: var " + tokens[x]);
+                            bw.write("Token: var " + tokens[x]);
+                            bw.newLine();
+                        } else {
+                            System.out.println("Token: " + tokens[x] + " = " + diccionario.getValor2(i));
+                            bw.write("Token: " + tokens[x] + " " + diccionario.getValor2(i));
+                            bw.newLine();
                         }
-                                
-                        }else{  
-                            
-                            System.out.println("Error: la expresion "+"'"+tokens[x]+"'"+" no existe."+diccionario.getValor2(i));
-                                
+
+                    } else {
+
+                        System.err.println("Error: la expresion " + "'" + tokens[x] + "'" + " no existe.");
+                        bw.write("Error: " + "(" + tokens[x] + ")" + " No existe en el diccionario.! ");
+                        bw.newLine();
                     }
-                     
+                }
+            }
+        } catch (IOException ex) {
+        }
+
+    }
+
+}
+
             
                    
                 }
